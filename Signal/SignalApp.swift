6b25c38780419d74c0880f7e222012382d759d0c
Leonhardmaster2 <leonhard.meingast@gmail.com>
2026-02-08 16:02:47 +0100

    Initial Commit

---
+++ Signal/SignalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignalApp.swift
+//  Signal
+//
+//  Created by Leonhard Meingast on 08.02.26.
+//
+
+import SwiftUI
+
+@main
+struct SignalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
